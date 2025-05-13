24202d27e0207972b7930c3f45100102099bcd39
Md. Ashikur Rahman <ashikur.learner@gmail.com>
2025-05-13 16:53:37 +0600

    basic c

--- nested_loop.c
+++ nested_loop.c
@@ -6,12 +6,14 @@ int main()
 
     int i, j;
 
-    for (i = 1; i <5; i++) {
+    for (i = 1; i <=5; i++) {
         printf("Office Day: %d\n", i);
-    }
 
+
+        //Inner loop
         for (j = 6; j <= 7; j++) {
             printf("Weekend: %d\n", j);
         }
+    }
 
 }
---
+++ while loop with while loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+
+    int i = 0;
+
+    while (i < 10) {
+      if (i == 4) {
+        break;
+      }
+      printf("%d\n", i);
+      i++;
+    }
+
+}
+
