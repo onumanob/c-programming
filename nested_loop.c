74af16beebc7cf9a16b95d8ae582680a6eaa6950
Md. Ashikur Rahman <ashikur.learner@gmail.com>
2025-05-13 15:14:27 +0600

    Nested loop practice

---
+++ nested_loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+    int i, j;
+
+    for (i = 1; i <5; i++) {
+        printf("Office Day: %d\n", i);
+    }
+
+        for (j = 6; j <= 7; j++) {
+            printf("Weekend: %d\n", j);
+        }
+
+}
